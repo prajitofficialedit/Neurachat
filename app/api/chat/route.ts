560b980cb7067d73c20d25ca49bd279fad101fd3
prajitofficialedit <prajitofficialedit@gmail.com>
2026-05-29 17:27:04 +0530

    fixed api

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,33 +1,15 @@
-import Groq from "groq-sdk";
-
-const groq = new Groq({
-  apiKey: process.env.GROQ_API_KEY,
-});
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
 
-    const response = await groq.chat.completions.create({
-      model: "llama-3.1-8b-instant",
-      messages: [
-        {
-          role: "system",
-          content: "You are NeuraChat AI.",
-        },
-        {
-          role: "user",
-          content: body.message,
-        },
-      ],
+    return NextResponse.json({
+      reply: "NeuraChat is working 🚀",
     });
-
-    return Response.json({
-      reply: response.choices[0]?.message?.content || "No response",
-    });
-  } catch (error: any) {
-    return Response.json({
-      reply: error.message,
+  } catch (error) {
+    return NextResponse.json({
+      reply: "Error",
     });
   }
 }
